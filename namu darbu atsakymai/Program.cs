2810cc9966702700e72e0fd4a8cb50d711518803
Rita <rita@trafi.com>
2018-06-28 17:53:52 +0300

    Viktorina

---
+++ namu darbu atsakymai/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Viktorina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int klausimuSk = 3;
+
+            int taskai = 0;
+
+            string[] klausimai = {
+                "Kokia Vengrijos sostine?",
+                "Kokios grupes albumas vadinas \"Dark side of the moon\"?",
+                "Kas parase romana \"Vargdieniai\"?"
+            };
+
+            string[,] galimiAtsakymai = {
+                {"Atenai", "Budapestas", "Praha"},
+                {"Led Zeppelin", "ABBA", "Pink Floyd"},
+                {"V. Hugo", "L. Tolstojus", "F. Dostojevskis"}
+            };
+
+            int[] teisingiAtsakymai = { 2, 3, 1 };
+
+            for (int i = 0; i < klausimuSk; i++)
+            {
+                Viktorina.AtspausdinkKlausimaSuAtsakymais(i, klausimai, galimiAtsakymai);
+                var atsakymas = Viktorina.NuskaitykAtsakyma();
+
+                if (atsakymas == teisingiAtsakymai[i])
+                {
+                    taskai += 3;
+                }
+                else
+                {
+                    var antrasAtsakymas = Viktorina.NuskaitykAtsakyma();
+                    if (antrasAtsakymas == teisingiAtsakymai[i])
+                    {
+                        taskai++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Is viso surinkote tasku: {taskai}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ namu darbu atsakymai/Viktorina.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Viktorina
+{
+    public static class Viktorina
+    {
+        public static void AtspausdinkKlausimaSuAtsakymais(int klausimoNr, 
+                                                           string[] klausimai, string[,] galimiAtsakymai)
+        {
+            Console.WriteLine(klausimai[klausimoNr]);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i + 1}. {galimiAtsakymai[klausimoNr, i]} ");
+            }
+        }
+
+        public static int NuskaitykAtsakyma() 
+        {
+            Console.WriteLine("\nIveskite atsakyma:");
+            var atsakymas = int.Parse(Console.ReadLine());
+            return atsakymas;
+        }
+    }
+}
